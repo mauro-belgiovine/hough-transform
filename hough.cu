8ef2112dff1fe1019ed8fe143fd68fb3d77f2f60
mauro-belgiovine <mauro.belgiovine@bo.infn.it>
2014-03-20 12:41:07 +0100

    block size changed in Tansform_GPU

--- hough.cu
+++ hough.cu
@@ -47,7 +47,7 @@
 #define DEG2RAD 0.017453293f
 
 #define SET_GRID_DIM(npoints, threadsPerBlock) ((npoints+(threadsPerBlock-1))/threadsPerBlock)
-#define BLOCK_DIM 32
+#define BLOCK_DIM 16
 
 using namespace std;
 
@@ -77,29 +77,33 @@ inline float stop_time(const char *msg) {
 //every CUDA Thread works processes a point of the input image
 __global__ void CudaTransform(unsigned char* dev_img, unsigned int *dev_accu, int w, int h){
   
-  //calculate params
-  float hough_h = ((sqrt(2.0) * (float)(h>w?h:w)) / 2.0);
-	  
-  float center_x = w/2;
-  float center_y = h/2;
   
   //calculate index which this thread have to process
   unsigned int index = getGlobalIdx_2D_2D();
-  //calculate coordinates for corresponding index in entire image
-  int x = index % w;
-  int y = index / w;
   
-  if( dev_img[index] > 250 ){ //se il punto è bianco (val in scala di grigio > 250)
-    for(int t=0;t<180;t++){ //plot dello spazio dei parametri da 0° a 180° (sist. polare)
+  //check index is in image bounds
+  if(index < (w*h)){
+    //calculate params
+    float hough_h = ((sqrt(2.0) * (float)(h>w?h:w)) / 2.0);
+	    
+    float center_x = w/2;
+    float center_y = h/2;
       
-      float r = ( ((float)x - center_x) * cos((float)t * DEG2RAD)) + (((float)y - center_y) * sin((float)t * DEG2RAD));
-      
-      //dev_accu[ (int)((round(r + hough_h) * 180.0)) + t]++;
-      atomicAdd(&(dev_accu[ (int)((round(r + hough_h) * 180.0)) + t]), 1);
-      
+    //calculate coordinates for corresponding index in entire image
+    int x = index % w;
+    int y = index / w;
+    
+    if( dev_img[index] > 250 ){ //se il punto è bianco (val in scala di grigio > 250)
+      for(int t=0;t<180;t++){ //plot dello spazio dei parametri da 0° a 180° (sist. polare)
+	
+	float r = ( ((float)x - center_x) * cos((float)t * DEG2RAD)) + (((float)y - center_y) * sin((float)t * DEG2RAD));
+	
+	//dev_accu[ (int)((round(r + hough_h) * 180.0)) + t]++;
+	atomicAdd(&(dev_accu[ (int)((round(r + hough_h) * 180.0)) + t]), 1);
+	
+      }
     }
   }
-  
   
 }
 
